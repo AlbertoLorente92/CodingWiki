85343c6de9da6deafbac71b0336d2d55cc576cf6
AlbertoLorente <alberto.lorente92@gmail.com>
2023-11-25 19:30:49 +0100

    Creating the tables Fluent_Author, Fluent_Book, Fluent_BookAuthorMap, Fluent_BookDetail, Fluent_Publisher with Fluent API

---
+++ CodingWiki_DataAccess/Migrations/20231125182220_addFluent_BooksToDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addFluent_BooksToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books_fluent",
+                columns: table => new
+                {
+                    IdBook = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ISBN = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books_fluent", x => x.IdBook);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books_fluent");
+        }
+    }
+}

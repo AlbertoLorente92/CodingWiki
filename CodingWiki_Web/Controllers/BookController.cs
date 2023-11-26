367f9b0d28a502fc84d7cde78769107b00c3c21e
AlbertoLorente <alberto.lorente92@gmail.com>
2023-11-26 19:33:37 +0100

    working with views and projections. Eager loading. IQueryable vs IEnumerable

--- CodingWiki_Web/Controllers/BookController.cs
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -1,7 +1,10 @@
 ﻿using CodingWiki_DataAccess.Data;
 using CodingWiki_Model.Models;
+using CodingWiki_Model.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace CodingWiki_Web.Controllers
 {
@@ -16,19 +19,33 @@ namespace CodingWiki_Web.Controllers
 
         public IActionResult Index()
         {
-            List<Book> objList = _db.Books.ToList();
+            List<Book> objList = _db.Books.Include(u => u.Publisher).ToList();  // bestway
+            //foreach(var obj in objList)
+            //{
+            //    // obj.Publisher = _db.Publishers.Find(obj.Publisher_id); // muy costoso
+            //
+            //    _db.Entry(obj).Reference(u => u.Publisher).Load();          // lo carga una vez y lo mantiene
+            //}
             return View(objList);
         }
 
 
         public IActionResult Upsert(int? id)
         {
-            Book obj = new();
+            BookVM obj = new();
+
+            obj.PublisherList = _db.Publishers.Select(cust =>  new SelectListItem
+            { 
+                Text = cust.Name,
+                Value = cust.Publisher_Id.ToString()
+            });
+
             if (id == null || id == 0)
             {
                 return View(obj);
             }
-            obj = _db.Books.FirstOrDefault(u => u.IdBook == id);
+
+            obj.Book = _db.Books.FirstOrDefault(u => u.IdBook == id);
             if (obj == null)
             {
                 return NotFound();
@@ -52,24 +69,118 @@ namespace CodingWiki_Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Upsert(Book obj)
+        public async Task<IActionResult> Upsert(BookVM obj)
         {
-            if(ModelState.IsValid) 
-            { 
-                if(obj.IdBook == 0)
-                {
-                    // create
-                    await _db.Books.AddAsync(obj);
-                }
-                else
-                {
-                    // update
-                    _db.Books.Update(obj);
-                }
-                await _db.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+
+            if(obj.Book.IdBook == 0)
+            {
+                // create
+                await _db.Books.AddAsync(obj.Book);
+            }
+            else
+            {
+                // update
+                _db.Books.Update(obj.Book);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            //BookVM obj = new();
+            //
+            //if (id == null || id == 0)
+            //{
+            //    return NotFound();
+            //}
+            //
+            //obj.Book = _db.Books.FirstOrDefault(u => u.IdBook == id);
+            //obj.Book.BookDetail1 = _db.BookDetails.FirstOrDefault(u => u.Book_Id == id);
+            //if (obj == null)
+            //{
+            //    return NotFound();
+            //}
+            //return View(obj);
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            BookDetail obj = new();
+            //obj.Book = _db.Books.FirstOrDefault(u => u.IdBook == id);
+            //obj = _db.BookDetails.FirstOrDefault(u => u.Book_Id == id);
+            obj = _db.BookDetails.Include(u => u.Book).FirstOrDefault(u => u.Book_Id == id);        //eager loading
+            if (obj == null)
+            {
+                return NotFound();
             }
             return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Details(BookDetail obj)
+        {
+            if (obj.BookDetail_Id == 0)
+            {
+                // create
+                await _db.BookDetails.AddAsync(obj);
+            }
+            else
+            {
+                // update
+                _db.BookDetails.Update(obj);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public async Task<IActionResult> PlayGround()
+        {
+            //updating related data
+            var bookDetails1 = _db.BookDetails.Include(b => b.Book).FirstOrDefault(b => b.BookDetail_Id == 4);
+            bookDetails1.NumberOfChapters = 2222;
+            bookDetails1.Book.Price = 222;
+            _db.BookDetails.Update(bookDetails1);
+            await _db.SaveChangesAsync();
+
+            var bookDetails2 = _db.BookDetails.Include(b => b.Book).FirstOrDefault(b => b.BookDetail_Id == 4);
+            bookDetails2.NumberOfChapters = 1111;
+            bookDetails2.Book.Price = 111;
+            _db.BookDetails.Attach(bookDetails2);
+            await _db.SaveChangesAsync();
+
+            //IEnumerable<Book> booklist1 = _db.Books;                // aplica el WHERE en memoria (lento)
+            //var filterBook1 = booklist1.Where(b => b.Price > 50).ToList();
+            //
+            //IQueryable<Book> booklist2 = _db.Books;                 // aplica el WHERE en bbdd (rapido)
+            //var filterBook2 = booklist2.Where(b => b.Price > 50).ToList();
+
+
+            //var bookTemp = _db.Books.FirstOrDefault();
+            //bookTemp.Price = 100;
+            //
+            //var bookCollection = _db.Books;
+            //decimal totalPrice = 0;
+            //
+            //foreach (var book in bookCollection)
+            //{
+            //    totalPrice += book.Price;
+            //}
+            //
+            //var bookList = _db.Books.ToList();
+            //foreach (var book in bookList)
+            //{
+            //    totalPrice += book.Price;
+            //}
+            //
+            //var bookCollection2 = _db.Books;
+            //var bookCount1 = bookCollection2.Count();
+            //
+            //var bookCount2 = _db.Books.Count();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
